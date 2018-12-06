864d611e55dc496e431a3a6a228fb5962c7c842d
MrLuiso <diaz.luispr@gmail.com>
2018-12-06 11:07:55 -0400

    Add better Styling for task comonent

--- src/components/TaskColumn.vue
+++ src/components/TaskColumn.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="taskColumn">
         <h2 class="title">{{columnTitle}}</h2>
-        <task-uncheked taskTitle="Title of Task"></task-uncheked>
+        <task-uncheked taskTitle="Title of Task" buttonText="Delete"></task-uncheked>
+        <task-uncheked taskTitle="Title of Task" buttonText="Delete"></task-uncheked>
+        <task-uncheked taskTitle="Title of Task" buttonText="Delete"></task-uncheked>
+        <task-uncheked taskTitle="Title of Task" buttonText="Delete"></task-uncheked>
     </div>
 </template>
 
--- src/components/TaskUnchecked.vue
+++ src/components/TaskUnchecked.vue
@@ -1,22 +1,34 @@
 <template>
     <div class="taskUnchecked">
-        <h3>{{taskTitle}}</h3>
+        <h3 id="tiitle">{{taskTitle}}</h3>
+        <b-button>{{buttonText}}</b-button>
     </div>    
 </template>
 
 <script>
 export default {
     name: 'TaskUnchecked',
-    props: ['taskTitle']
+    props: ['taskTitle', 'buttonText']
 }
 </script>
 
 <style scoped>
-.taskUncheked{
+.taskUnchecked{
     display: flex;
     flex: 1;
     flex-direction: row;
-    justify-content: flex-start;
-    background-color: aqua;
+    justify-content: space-between;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-top: 20px;
+}
+
+#title{
+    display: flex;
+}
+
+#deleteButton{
+    display: flex;
+    justify-self: flex-end;
 }
 </style>
